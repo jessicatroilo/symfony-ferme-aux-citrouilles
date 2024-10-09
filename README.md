e68f59e58bd513cf90732c114d3152304824c01d
jessicatroilo <jessica.troilo25@gmail.com>
2024-10-09 12:28:55 +0200

    initialisation nav and project

---
+++ README.md
@@ -0,0 +1,13 @@
+# Site fictif de e-commerce - Symfony
+
+## In progress - Projet en cours de construction :
+
+- Présentation une ferme et ses activités, 
+- Achat en ligne possible pour plusieurs services,
+- Création d'un Back-Office et d'un Front-Office.
+
+### Techno : 
+- Symfony
+- Twig
+- Tailwindcss
+- MariaDB/SQL
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./assets/**/*.js",
+    "./templates/**/*.html.twig",
+  ],
+  theme: {
+    colors: {
+      'pearl': "#fbfcf8",
+      'pumpkin': "#fdaa57",
+      'leaf': "#739d7d",
+      'farm-blue': "#3f646f",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
