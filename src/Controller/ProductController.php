40da5f9fdd88485e3394f693b83cc19d5bcbd171
jessicatroilo <jessica.troilo25@gmail.com>
2024-12-10 13:35:14 +0100

    update product detail

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -31,5 +32,18 @@ class ProductController extends AbstractController
             'productList' => $allProducts,
         ]);
     }
-}
 
+    /**
+     * Method to show product details
+     * 
+     * @param Product $product
+     * @return Response
+     */
+    #[Route('/{id}', name: 'show', methods: ['GET'], requirements: ['id' => '\d+'])]
+    public function show(Product $product): Response
+    {
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+}
