ca491f1d4fb75770b9033801735cf1adbd5194bb
jessicatroilo <jessica.troilo25@gmail.com>
2024-12-11 18:34:14 +0100

    feat(templates/form): mise en forme via form et twig

--- src/Form/SearchProductType.php
+++ src/Form/SearchProductType.php
@@ -29,11 +29,17 @@ class SearchProductType extends AbstractType
             '10' => 10,
             '50' => 50
         ],
-        'label' => false,
+        'label' => 'Produit(s) par page : ',
+        'attr' => [
+            'class' => 'p-1'
+        ],
+        'data' => $_GET['productsPerPage'] ?? 5 // Valeur par défaut - en récupérant la valeur de la query string via $_GET et ?? pour la valeur par défaut si la query string n'existe pas
         
         ])
         ->add('submit', SubmitType::class, [
             'label' => 'Afficher',
+            'attr' => ['class' => 'flex self-stretch lg:self-start justify-center mt-1 lg:text-lg text-m font-bold bg-leaf text-pearl p-2 rounded-lg hover:ring-2 ring-pumpkin hover:bg-farm-blue'
+            ]
         ]);
     
     }
