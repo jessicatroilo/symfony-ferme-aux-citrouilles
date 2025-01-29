4b390cba90c1bf465f0c01c046e34e618cce3ca6
jessicatroilo <jessica.troilo25@gmail.com>
2025-01-29 13:20:22 +0100

    feat(src/controller) : add logic in controller for receive contact message

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Contact;
 use App\Form\ContactType;
+use Symfony\Component\Mime\Email;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -22,38 +24,57 @@ class ContactController extends AbstractController
      * @return Response
      */
     #[Route('/contact', name: 'app_front_contact', methods: ['GET', 'POST'])]
-    public function contact(Request $request, Contact $contact, EntityManagerInterface $entityManager): Response
+    public function contact(Request $request, Contact $contact, EntityManagerInterface $entityManager, MailerInterface $mailer ): Response
     {
+        // création d'une nouvelle instance de l'entité Contact
+        $contact = new Contact(); 
 
-        $contact = new Contact();
+        //donnée de test // TODO à supprimer 
+        $contact->setFirstname('Jessica');
+        $contact->setLastname('Troilo');
+        $contact->setEmail('jessica.troilo@yahoo.fr');
+        $contact->setSubject('Test');
+        $contact->setMessage('Ceci est un message de test');
 
-        $form = $this->createForm(ContactType::class, $contact);
-        $form->handleRequest($request);
+        // Création du formulaire
+        $form = $this->createForm(ContactType::class, $contact); 
 
+        // récupération des données du formulaire
+        $form->handleRequest($request); 
+
+        // Si le formulaire est soumis et valide
         if ($form->isSubmitted() && $form->isValid()) {
 
-            $contact->setCreatedAt();
+            $contact->setCreatedAt(); // ajout de la date de création en BDD
 
-            // Save the contact in the database
+            // Save the contact in the database if you have database
             $entityManager->persist($contact);
             $entityManager->flush();
 
 
-            // TODO - à faire fonctionner - Send an email to the admin
-            $email = (new TemplatedEmail())
-                ->from($contact->getEmail())
-                ->to('jessica.troilo25@gmail.com') 
-                ->subject($contact->getSubject())
-                ->htmlTemplate('emails/contact.html.twig')
-                ->context([
-                    'contact' => $contact,
-                ]);
-                ;
-            $mailer->send($email);
+             // Gestion des erreurs
+            try {    
+                $email = (new TemplatedEmail()) // contient les mêmes méthodes que Email() avec la possibilité de passer un template twig
+                    ->from($contact->getEmail())
+                    ->to('jessica.troilo25@gmail.com') 
+                    ->subject($contact->getSubject())
+                    ->text($contact->getMessage())
+                    ->htmlTemplate('emails/contact.html.twig') // template twig
+                    ->context([
+                        'contact' => $contact,
+                    ]); // données à passer au template twig
+        
+                $mailer->send($email);
 
-            $this->addFlash('success', 'Votre message a bien été envoyé');
+                // Message de confirmation
+                $this->addFlash('success-contact', 'Votre message a bien été envoyé');
 
-            return $this->redirectToRoute('app_home');
+                // Redirection vers la page de contact
+                return $this->redirectToRoute('app_front_contact');
+            } catch (\Exception $e) {
+                $this->addFlash('error-contact', 'Une erreur est survenue lors de l\'envoi du message');
+            }  
+
         }
 
         return $this->render('/contact/contact.html.twig', [
